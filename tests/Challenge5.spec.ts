25abec61eb855d1cdeb0846a27d778eae64caa6f
vinayrk77 <vinayrk26@gmail.com>
2026-07-03 17:50:26 +0530

    dropdowns and date picker

--- tests/Challenge5.spec.ts
+++ tests/Challenge5.spec.ts
@@ -91,6 +91,142 @@ test("Verify automation page", async({page})=>{
         }
     }
 
+    //dropdowns
+    const dropdown = page.locator('#country');
+    await expect(dropdown).toBeVisible();
+    await dropdown.selectOption('Germany');
+
+    //verify number of options
+    const dropdownOptions = page.locator('#country option');
+    const count = await dropdownOptions.count();
+    console.log("Number of dropdown options are:", count);
+    expect(count).toBe(10);
+
+    //verify option prestent
+    const dropdownText = (await dropdownOptions.allTextContents()).map(text=>text.trim());
+    console.log("all options are:",dropdownText);
+    expect(dropdownText).toContain("Japan");
+    //print all drop down
+    for(let text of dropdownText)
+    {
+        console.log(text);
+    }
+
+    //Multi Select dropdown - select 3 options
+    const colorsdropdown = page.locator('#colors');
+    await expect(colorsdropdown).toBeVisible();
+    await colorsdropdown.selectOption(['green', 'yellow', 'white']);
+    await expect(colorsdropdown).toHaveValues(['green', 'yellow', 'white']);
+
+    //count number of options
+    const colorsOptions = page.locator('#colors option');
+    const colorsCount = await colorsOptions.count();
+    console.log("number of options are:", colorsCount);
+    expect(colorsCount).toBe(7);
+    // verify option present
+    const colorsText = (await colorsOptions.allTextContents()).map(text=>text.trim());
+    console.log(colorsText);
+    expect(colorsText).toContain('Blue')
+
+    for(let text of colorsText)
+    {
+        console.log(text);
+    }
+    //check if arrays are sorted or not
+    const originalList:string[] = [...colorsText];
+    const sortedList: string[] = [...colorsText].sort();
+    console.log("original List is:", originalList);
+    console.log("sorted List is:", sortedList);
+    
+    if(JSON.stringify(originalList) === JSON.stringify(sortedList))
+    {
+        console.log("Arrays are sorted");
+    }
+    else
+    {
+        console.log("arrays are not sorted");
+    }
+
+    //check if dropdown has duplicates
+    const myset = new Set<string>;
+    const duplicates: string[] = [];
+
+    for(let text of colorsText)
+    {
+        if(myset.has(text))
+        {
+            duplicates.push(text);
+        }
+        else
+        {
+            myset.add(text);
+        }
+    }
+    console.log("Duplicate options are:",duplicates);
+
+    //select any options from animals dropdown 
+    const animaldropdown = page.locator('select#animals');
+    await expect(animaldropdown).toBeVisible();
+    await animaldropdown.selectOption(['Cheetah', 'Deer', 'Dog']);
+
+    const animalOptions = page.locator('select#animals option');
+    const animalCount = await animalOptions.count();
+    console.log("Animal Options count is:",animalCount);
+    expect(animalCount).toBe(10);
+
+    const animalText = await animalOptions.allInnerTexts();
+    console.log(animalText);
+    expect(animalText).toContain('Rabbit');
+
+    //check if sorted
+    const originalAnimal:string[] = [...animalText];
+    const sortedAnimal:string[] = [...animalText].sort();
+    expect(originalAnimal).toEqual(sortedAnimal);
+
+    // select date Picker
+    const datePicker = page.locator('input#datepicker');
+    await expect(datePicker).toBeVisible();
+    await expect(datePicker).toBeEnabled();
+    await datePicker.click();
+
+    const month:string = 'December';
+    const year:string = '2027';
+    const date:string = '31';
+    
+
+    while(true)
+    {
+        const currentMonth = await page.locator('.ui-datepicker-month').innerText();
+        const currentYear = await page.locator('.ui-datepicker-year').innerText();
+
+        if(currentMonth === month && currentYear === year)
+        {
+            break
+        }
+        else
+        {
+            page.locator('.ui-datepicker-next').click();
+        }
+    }
+    const alldates = await page.locator('.ui-datepicker-calendar a').all();
+   
+    for(let dt of alldates)
+    {
+        const dateText = await dt.innerText()
+        if(dateText === date)
+        {
+            await dt.click();
+            break;
+        }
+    }
+    const expectedDate = '12/31/2026';
+    await expect(datePicker).toHaveValue(expectedDate);
+    
+
+
+
+
+    
 
 
 });
